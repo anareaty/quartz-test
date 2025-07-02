24818d783b5964d861ce457dff516b7f734e8d63
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-02 13:18:00 +0400

    Update Profile.tsx

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -2,9 +2,14 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const Profile: QuartzComponent = () => {return <img src="avatar.jpg">}
-
-
+const Profile: QuartzComponent = () => {
+  const title = "fff"
+  if (title) {
+    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+  } else {
+    return null
+  }
+}
 Profile.css = `
 .article-title {
   margin: 2rem 0 0 0;
