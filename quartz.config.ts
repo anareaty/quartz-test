0fb73190241b86c060cd6e24b3f1d41bb0ceda5c
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-02 12:13:27 +0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,6 +43,7 @@ const config: QuartzConfig = {
         darkMode: {
           light: "#161618",
           lightgray: "#393639",
+          fadedgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
