9691af9fbeffe9699f10f3ad4d283f99708fcc4c
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-02 13:01:26 +0400

    Update Profile.tsx

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -4,12 +4,6 @@ import { classNames } from "../util/lang"
 
 const Profile: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   return <img src="">
-  const title = fileData.frontmatter?.title
-  if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
-  } else {
-    return null
-  }
 }
 
 Profile.css = `
