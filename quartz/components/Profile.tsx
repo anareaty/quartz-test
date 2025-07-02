406f01f8c10868fec1a6d030eb26b221829b87ed
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-02 15:00:19 +0400

    Update Profile.tsx

--- quartz/components/Profile.tsx
+++ quartz/components/Profile.tsx
@@ -1,6 +1,7 @@
 
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import * as Component from "./quartz/components"
 
 const Profile: QuartzComponent = () => {
   return <div> 
