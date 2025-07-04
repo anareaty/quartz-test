d3e35eebdc8bb365ea8d743e58d570b988eff4bc
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-04 07:28:32 +0400

    Update NavigationLinks.tsx

--- quartz/components/NavigationLinks.tsx
+++ quartz/components/NavigationLinks.tsx
@@ -2,10 +2,11 @@ import { FullSlug, resolveRelative } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const NavigationLinks: QuartzComponent = () => {
+const NavigationLinks: QuartzComponent = ({fileData} : QuartzComponentProps) => {
+  const linkDest = resolveRelative(fileData.slug!, `feed/1` as FullSlug)
   return (
     <li>
-      <a href='/feed'>
+      <a href={linkDest}>
         Лента
       </a>
     </li>
