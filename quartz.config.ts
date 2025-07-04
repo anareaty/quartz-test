48a5b4f03d0033731e143d8d736f32fca74b0dc1
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2025-07-04 09:10:34 +0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "anareaty.github.io/quartz-test",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
